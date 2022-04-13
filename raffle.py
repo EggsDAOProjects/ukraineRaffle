9b1491f9cf4993554b21fca007fa7adbddafdf4f
Adrian Henry <adrian@hungryturtlecode.com>
2022-04-13 13:00:06 +0100

    add support for drawing multiple winners from a single random seed

--- raffle.py
+++ raffle.py
@@ -3,16 +3,27 @@ import random
 import sys
 
 
-def pick_winner(tickets):
-    return random.choice(tickets)
+def pick_winner(tickets, winners=1):
+    all_winners = []
+
+    for i in range(winners):
+        all_winners.append(random.choice(tickets))
+
+    return all_winners
 
 
 if __name__ == '__main__':
     seed = sys.argv[1]
     random.seed(seed)
 
+    winners = 1
+    if len(sys.argv) > 2:
+        winners = int(sys.argv[2])
+
     with open('./raffle_entries.json') as f:
         tickets = json.loads(f.read())
 
-    winner = pick_winner(tickets)
-    print(winner)
+    all_winners = pick_winner(tickets, winners)
+
+    for i, winner in enumerate(all_winners):
+        print(f"{i+1}.", winner)
